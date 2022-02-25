96f6a523c534b987bd559ad7f0cd00d39bbda7ac
Claris <claris@jadelabs.org>
2022-02-25 10:04:38 +0700

    fix Marketplace homepage

--- src/views/Home/components/MarketplaceItem.tsx
+++ src/views/Home/components/MarketplaceItem.tsx
@@ -21,13 +21,12 @@ const MarketplaceItem = (prop) => {
 }
 
 const Item = styled(Box)`
-    text-align: center;
     padding-left: 15px;
     padding-right: 15px;
     height: 100%;
-    display: flex;
+    display: inline-flex;
     flex-direction: column;
-    justify-content: end;
+    justify-content: space-between;
 
     @media only screen and (max-width: 767px) {
         max-width: 100%;
@@ -42,6 +41,7 @@ const ModuleBorderWrap = styled(Box)`
     width: 250px;
     position: relative;
     padding: 3px;
+    align-self: flex-end;
     background: linear-gradient(102.16deg, #00FDFF 0%, #22C3FF 3.86%, #4190FF 7.78%, #5B64FF 11.78%, #7040FF 15.78%, #8124FF 19.78%, #8D10FF 23.79%, #9404FF 27.81%, #9600FF 31.88%, #00FDFF 64.8%, #22C3FF 69.06%, #4190FF 73.39%, #5B64FF 77.8%, #7040FF 82.22%, #8124FF 86.64%, #8D10FF 91.06%, #9404FF 95.51%, #9600FF 100%);
 `
 
@@ -62,6 +62,8 @@ const Button = styled.button`
 const Img = styled.img`
     margin: auto;
     margin-bottom: 60px;
+    flex: 1;
+    object-fit: contain;
 `
 
 export default MarketplaceItem
