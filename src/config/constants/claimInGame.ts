a23c6af641d0d6297f671301704eaa7a2a0b4c17
hlhdai <dai.lh.huynh@gmail.com>
2022-07-13 16:09:49 +0700

    feat: config for prod

--- src/config/constants/claimInGame.ts
+++ src/config/constants/claimInGame.ts
@@ -3,7 +3,7 @@ import cliamBIG from 'config/abi/cliamBIG.json'
 const claimInGameConfig = {
     contractAddress: {
         97: "0x568B7aC5828C5b5e9c99B26657E1AaA7bC0bD9a8",
-        56: "",
+        56: "0xf696bed60217ec5f70aD81654921eC6A8b60439C",
     },
     abi: cliamBIG
 }
