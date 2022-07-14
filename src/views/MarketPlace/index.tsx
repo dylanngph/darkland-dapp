72c6429e50e359e9b4a0d43bccfabf1443d5fdc2
hlhdai <dai.lh.huynh@gmail.com>
2022-07-14 16:41:30 +0700

    fix: marketplace not load

--- src/views/MarketPlace/index.tsx
+++ src/views/MarketPlace/index.tsx
@@ -14,11 +14,15 @@ import HeroesTab from './components/tabs/HeroesTab/HeroesTab'
 import { tabMarket } from './constant'
 
 const MarketPlace = () => {
-  const dispatch = useAppDispatch()
-  const { t } = useTranslation()
+  // const dispatch = useAppDispatch()
+  // const { t } = useTranslation()
   // const { itemConfig, runeConfig } = useSelector((state: AppState) => state.common)
-  const tabSelected = useSelector<AppState, AppState['user']['userSelected']>((state: AppState) => state.user.userSelected)
-  const [tab, setTab] = useState(tabSelected)
+  // const tabSelected = useSelector<AppState, AppState['user']['userSelected']>(
+  //   (state: AppState) => state.user.userSelected,
+  // )
+  // const [tab, setTab] = useState(tabSelected)
+  // console.log('🚀 ~ file: index.tsx ~ line 21 ~ MarketPlace ~ tabSelected', tabSelected)
+  // console.log('🚀 ~ file: index.tsx ~ line 24 ~ MarketPlace ~ tab', tab)
 
   // useEffect(() => {
   //   return () => {
@@ -49,93 +53,93 @@ const MarketPlace = () => {
   //   }
   // }, [dispatch, itemConfig, runeConfig])
 
-  useEffect(() => {
-    setTab(tabSelected)
-  }, [tabSelected])
+  // useEffect(() => {
+  //   setTab(tabSelected)
+  // }, [tabSelected])
 
-  
-  const handleChangeTab = (index: any) => {
-    setTab(index) 
-    dispatch(updateSelected(index))
-  }
+  // const handleChangeTab = (index: any) => {
+  //   setTab(index)
+  //   dispatch(updateSelected(index))
+  // }
 
-  const renderTabHeader = () => (
-    <Tabs
-      sx={{
-        color: '#9E9E9E',
-        borderColor: '#686868',
-      }}
-      defaultIndex={tabSelected}
-    >
-      <TabList>
-        {tabMarket.map((tabItem, index) => {
-          return (
-            <TabCustom
-              aria-hidden="true"
-              key={tabItem.label}
-              onClick={() => {
-                handleChangeTab(index)
-              }}
-              sx={{
-                width: '150px',
-                paddingTop: '0px',
-              }}
-              style={tab === tabItem.index ? styleActive : {}}
-              _focus={{ border: 'unset' }}
-              _active={{ border: 'unset' }}
-            >
-              {tabItem.label}
-            </TabCustom>
-          )
-        })}
-      </TabList>
-    </Tabs>
-  )
+  // const renderTabHeader = () => (
+  //   <Tabs
+  //     sx={{
+  //       color: '#9E9E9E',
+  //       borderColor: '#686868',
+  //     }}
+  //     defaultIndex={tabSelected}
+  //   >
+  //     <TabList>
+  //       {tabMarket.map((tabItem, index) => {
+  //         return (
+  //           <TabCustom
+  //             aria-hidden="true"
+  //             key={tabItem.label}
+  //             onClick={() => {
+  //               handleChangeTab(index)
+  //             }}
+  //             sx={{
+  //               width: '150px',
+  //               paddingTop: '0px',
+  //             }}
+  //             style={tab === tabItem.index ? styleActive : {}}
+  //             _focus={{ border: 'unset' }}
+  //             _active={{ border: 'unset' }}
+  //           >
+  //             {tabItem.label}
+  //           </TabCustom>
+  //         )
+  //       })}
+  //     </TabList>
+  //   </Tabs>
+  // )
 
-  const renderTabContent = useCallback(() => {
-    const tabs = {
-      // 0: <BoxesTab />,
-      1: <HeroesTab />,
-      // NFT: <Runes />,
-    }
-    return (
-      tabs[tab] || (
-        <div
-          style={{
-            fontSize: 30,
-            height: 300,
-            justifyContent: 'center',
-            alignItems: 'center',
-            display: 'flex',
-          }}
-        >
-          Coming soon
-        </div>
-      )
-    )
-  }, [tab])
+  // const renderTabContent = useCallback(() => {
+  //   const tabs = {
+  //     // 0: <BoxesTab />,
+  //     1: <HeroesTab />,
+  //     // NFT: <Runes />,
+  //   }
+  //   return (
+  //     tabs[tab] || (
+  //       <div
+  //         style={{
+  //           fontSize: 30,
+  //           height: 300,
+  //           justifyContent: 'center',
+  //           alignItems: 'center',
+  //           display: 'flex',
+  //         }}
+  //       >
+  //         Coming soon
+  //       </div>
+  //     )
+  //   )
+  // }, [tab])
 
   return (
     <Page>
-      {/* {renderTabHeader()} */}
-      {renderTabContent()}
+      {/* {renderTabHeader()}
+      {renderTabContent()} */}
+      <HeroesTab />
     </Page>
   )
 }
-const styleActive = {
-  color: 'white',
-  marginBottom: '-2px',
-  borderBottom: '4px solid #FE335B',
-}
+// const styleActive = {
+//   color: 'white',
+//   marginBottom: '-2px',
+//   borderBottom: '4px solid #FE335B',
+// }
 
-const TabCustom = styled(Tab)`
-  margin: 0 50px;
-  @media screen and (max-width: 768px) {
-    margin: 20px;
-  }
-  @media screen and (max-width: 440px) {
-    margin: 0px;
-  }
-`
+// const TabCustom = styled(Tab)`
+//   margin: 0 50px;
+//   @media screen and (max-width: 768px) {
+//     margin: 20px;
+//   }
+//   @media screen and (max-width: 440px) {
+//     margin: 0px;
+//   }
+// `
 
 export default MarketPlace
